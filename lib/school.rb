372b874191edec9f4715c3f7effc39acae2e86c6
Cameron Hudson <camnn3@yahoo.com>
2020-07-06 16:59:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  
+  attr_accessor
+  attr_reader :roster
+  def initialize(roster)
+    if @roster = roster
+      @roster = {}
+    end
+    
+    def add_student(student, grades)
+      if @roster.include?grades
+        
